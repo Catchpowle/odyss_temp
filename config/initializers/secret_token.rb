9f5a2d1819d4eb1d73262a9ff56ec26250d59a2e
Jonathan Catchpowle <jonathan.catchpowle@gmail.com>
2014-08-03 18:21:52 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Odyss::Application.config.secret_key_base = 'bb95fcd0d2d51a093a47efad2be6f0c98c097248ab864b440d7bd29784632be2124e46f49bf9ea335c15bdd6fb844cb95d91512a313a092b23bced2df7a5fc03'
